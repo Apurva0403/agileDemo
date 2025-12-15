266bcb42690daf626c6fca5d63c0e0b668169623
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2025-12-15 12:22:16 +0530

    New file added

---
+++ happy.java
@@ -0,0 +1,7 @@
+public class happy{
+    public static void main(String [] args){
+        int num1=5,num2=89, sum;
+        sum= num1 + num2;
+        System.out.println("sum of these numbers are :"+sum);
+    }
+}
